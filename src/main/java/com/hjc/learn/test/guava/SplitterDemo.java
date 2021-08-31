849aebb260bdf75a424b7768e56f8590efa6845c
jackjchou <jackjchou@tencent.com>
2021-08-31 11:26:50 +0800

    guava Splitter + 正则

---
+++ src/main/java/com/hjc/learn/test/guava/SplitterDemo.java
@@ -0,0 +1,36 @@
+package com.hjc.learn.test.guava;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author houjichao
+ */
+public class SplitterDemo {
+
+    public static void main(String[] args) {
+        String sql = "select * from user LIMIT 1 limit 2";
+
+        System.out.println(Splitter.on("limit").splitToList(sql).get(0));
+
+        System.out.println(Splitter.onPattern("(?i)limit").splitToList(sql).get(0));
+
+
+        System.out.println(Splitter.on(Pattern.compile("^limit$", Pattern.CASE_INSENSITIVE)).splitToList(sql).get(0));
+
+        String regex1 = "^(?i)limit$";
+
+        String regex = "^limit$";
+        String s = "limit1";
+        System.out.println(s.matches(regex1));
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        System.out.println(p.matcher(s).matches());
+
+        System.out.println(Splitter.onPattern(regex1).splitToList(sql).get(0));
+
+
+    }
+
+}
